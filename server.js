ba37d788cc49ade6ed60f261620f528b82dc59b8
Rgaumond <101813868+Rgaumond@users.noreply.github.com>
2023-01-04 16:13:07 -0500

    port to PORT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.port || 5510;
+const port = process.env.PORT || 5510;
 const workoutRouter = require("./routes/workout");
 const exerciseRouter = require("./routes/exercise");
 const cors = require("cors");
